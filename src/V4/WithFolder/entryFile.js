7a7ee0885ca71613cdd1c35cfca1e19da9f2337f
sunildabbi <sunildabbi03@gmail.com>
2025-06-17 13:15:29 +0530

    Withtable Insertfooter implemented in frontend extension.

--- src/V4/WithFolder/entryFile.js
+++ src/V4/WithFolder/entryFile.js
@@ -1,9 +1,10 @@
 const { StartFunc: StartFromShowAllGet } = require("./ShowAllGet/entryFile");
 const { StartFunc: StartFromShow } = require("./Show/entryFile");
-
+const { StartFunc: StartInsertFooter } = require("./Crud/entryFile");
 const StartFunc = () => {
     StartFromShowAllGet();
     StartFromShow();
+    StartInsertFooter();
 };
 
 module.exports = { StartFunc };
