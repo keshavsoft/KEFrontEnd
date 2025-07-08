b2f5ee0ceb218d6c8c4499456c53f4e49efead8d
keshavsoft <nknnkumar@yahoo.com>
2025-07-08 10:36:01 +0530

    TableShow started

--- src/V7/DomContentLoad/CommonConfig/entryFile.js
+++ src/V7/DomContentLoad/CommonConfig/entryFile.js
@@ -1,9 +1,11 @@
 const { StartFunc: StartFuncFromCreateNoValid } = require("./CreateNoValid/entryFile");
 const { StartFunc: StartFuncFromTableWithCrud } = require("./TableWithCrud/entryFile");
+const { StartFunc: StartFuncFromTableShow } = require("./TableShow/entryFile");
 
 const StartFunc = () => {
     StartFuncFromCreateNoValid();
     StartFuncFromTableWithCrud();
+    StartFuncFromTableShow();
 };
 
 module.exports = { StartFunc };
